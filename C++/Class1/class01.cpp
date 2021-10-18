1b2a4bf9173290814b7ec54be7648e880f516b58
AllenLee3639 <35442960+AllenLee3639@users.noreply.github.com>
2021-10-18 23:27:28 +0800

    C++
    第一课

---
+++ C++/Class1/class01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+
+
+using namespace std;
+
+int main(){
+	char c = 65;																		
+	char d = 67;
+	char e = 67;
+	cout<<c<<d<<e<<endl;
+	return 0;
+}
---
+++ C++/Class1/class1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<bitset>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){	
+//	string s;
+//	int age; 
+//	cout << "请输入您的名称： ";
+//	cin >> s;
+//	cout << "请输入您的年龄： ";
+//	cin >> age; 
+//	cout<<"我是"<<s<<", 我今年"<<age<<"岁了！"<<endl;
+
+//	int a = 137;
+//	char c = 'a';
+//	string s = "Hello";
+//	bool b = true;
+//	float f = 1.9;
+//	double d = 2.0;
+//	
+//	cout<<sizeof(a)<<endl;	
+//	cout<<sizeof(c)<<endl;	
+//	cout<<sizeof(s)<<endl;	
+//	cout<<sizeof(b)<<endl;
+//	cout<<sizeof(f)<<endl;	
+//	cout<<sizeof(d)<<endl;
+	
+//	int a = 5;
+//	int b = 2;
+//	
+//	cout<<a%b<<endl;	
+//	return 0;
+}
